13425a3bd5c48344b9067e0b946432a0025964ce
TheClockTwister <leon.wiesen@rwth-aachen.de>
2024-08-23 01:09:13 +0200

    add assembly info and icon for windows

---
+++ AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FWtest")]
+[assembly: AssemblyProduct("Firewall Tester")]
+// [assembly: AssemblyConfiguration("Configuration")]
+[assembly: AssemblyCompany("TheClockTwister")]
+[assembly: AssemblyCopyright("©2024 TheClockTwister. Distributed under GPL-3.0.")]
+[assembly: AssemblyTrademark("Distributed under GPL-3.0.")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0")]
+[assembly: ObfuscateAssembly(assemblyIsPrivate: true, StripAfterObfuscation = true)]
+
+[assembly: ComVisible(false)]
